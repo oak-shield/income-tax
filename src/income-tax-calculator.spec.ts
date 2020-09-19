1c8b7962a5ea29c471c9b8fd92d337f6fd966c0b
igor Magro <iggormagro8@gmail.com>
2020-09-18 23:30:29 -0300

    updated error image

--- src/income-tax-calculator.spec.ts
+++ src/income-tax-calculator.spec.ts
@@ -7,28 +7,28 @@ describe('income tax validator', () => {
 
   /* Apenas os testes:
 
-    - test('should incomeTax==600 if salary > 2000')
-    - test('should incomeTax==225 if salary > 1000 && salario <= 2000')
-    - test('should incomeTax==0 if salary < 1000')
+    - test('should incomeTax==600 if salary == 4000')
+    - test('should incomeTax==225 if salary == 1500')
+    - test('should incomeTax==0 if salary == 500')
 
-    Cobrem todas as sentenças (Stmts) e ramos (Branch) do código
+    Cobrem todas as sentenças (Stmts) e ramos (Branch) do código, no entanto revelou um defeito na implementação
   */
 
-  test('should incomeTax==600 if salary > 2000', () => {
+  test('should incomeTax==600 if salary == 4000', () => {
     const salary = 4000
     const incomeTax = incomeTaxCalculator.getIncomeTax(salary)
     expect(incomeTax).toEqual(550)
   })
 
-  test('should incomeTax==225 if salary > 1000 && salario <= 2000', () => {
+  test('should incomeTax==225 if salary == 1500', () => {
     const salary = 1500
     const incomeTax = incomeTaxCalculator.getIncomeTax(salary)
     expect(incomeTax).toEqual(75)
   })
 
-  test('should incomeTax==0 if salary < 1000', () => {
-    const salary = 1500
+  test('should incomeTax==0 if salary == 500', () => {
+    const salary = 500
     const incomeTax = incomeTaxCalculator.getIncomeTax(salary)
-    expect(incomeTax).toEqual(75)
+    expect(incomeTax).toEqual(0)
   })
 })
